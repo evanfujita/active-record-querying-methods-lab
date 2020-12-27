37f013e85b7edcf078f1f6e17ddaaf5dfac32094
Evan <evanfujita@gmail.com>
2020-12-27 09:37:40 -0600

    Done.

---
+++ db/seeds.rb
@@ -0,0 +1,4 @@
+
+show = Show.create(name: "Twin Peaks", network: "CBS", day: "Wednesday", rating: "8", season: "two")
+show1 = Show.create(name: "Arrested Development", network: "FOX", day: "Tuesday", rating: "9", season: "four")
+show2 = Show.create(name: "High Castle", network: "AP", day: "Sunday", rating: "6", season: "four")
